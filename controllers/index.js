ee33726c2d5b9f6e93b9e10cbec10a28af7dcaa9
kylezap <kzapcic+github@gmail.com>
2024-05-01 14:46:20 -0400

    form update

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
+
 //router.use('/api', apiRoutes);
 
 module.exports = router;
