88c90fe53c0a4e14d5eb73c3bd0a03e27bc37c57
Sam Hazinski <shazinski@gmail.com>
2024-05-02 12:48:49 -0400

    added calorie goal calorieGoalRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const recipesRoutes = require('./recipesRoutes');
-// const calorieGoalRoutes = require('./calorieGoalRoutes');
+const calorieGoalRoutes = require('./calorieGoalRoutes');
 const userRecipesRoutes = require('./userRecipesRoutes');
 
 
 router.use('/users', userRoutes);
 router.use('/recipes', recipesRoutes);
-// router.use('/calorieGoal', calorieGoalRoutes);
+router.use('/calorieGoal', calorieGoalRoutes);
 router.use('/userRecipes', userRecipesRoutes);
 
 module.exports = router;
