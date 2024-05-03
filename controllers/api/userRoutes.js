4c878a9b506f0bbbe68658a8787befab8da6188d
kylezap <kzapcic+github@gmail.com>
2024-05-02 22:16:54 -0400

    login/logout update fixed the logout target in handlebars so logout works

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -87,6 +87,7 @@ router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
+      res.render
     });
   } else {
     res.status(404).end();
